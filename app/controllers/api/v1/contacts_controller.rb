8ddfda11d2168ead5a8ba91b537dcf198470fedc
cvolpe12 <christopher.volpe12@gmail.com>
2019-08-19 14:30:23 -0400

    added rails app

---
+++ app/controllers/api/v1/contacts_controller.rb
@@ -0,0 +1,43 @@
+class Api::V1::ContactsController < ApplicationController
+  before_action :find_contact, only: [:update]
+
+  def index
+    @contacts = Contact.all
+    render json: @contacts
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      render json: @contact, status: :accepted
+    else
+      render json: { errors: @contact.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  def update
+    @contact.update(contact_params)
+    if @contact.save
+      render json: @contact, status: :accepted
+    else
+      render json: { errors: @contact.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id]).destroy
+
+  end
+
+  private
+
+  def contact_params
+    params.permit(:name, :age, :species, :breed, :hobbies, :plays_well, :picture, :bio)
+  end
+
+  def find_contact
+    @contact = Contact.find(params[:id])
+  end
+end
+
+end
